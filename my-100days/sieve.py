060bff8b918c24a0edd62fcf1289bd26df609662
Radu Stoenescu <radu.stoe@gmail.com>
2019-05-06 12:53:28 +0300

    + better var names and comments

--- my-100days/sieve.py
+++ my-100days/sieve.py
@@ -11,7 +11,6 @@ def sieve():
         picker = filter(lambda x, prime = v: x % prime != 0, picker)
 
 import heapq
-
 # In the proper sieve we will cross multiples as we go,
 # For each prime we found we'll keep its largest multiple generated
 # that wasn't reached so far in our search.
@@ -20,24 +19,29 @@ import heapq
 # multiple is inserted in the heap.
 def proper_sieve():
     yield 2
-    crt = 3
-    cross_list = [(4,2)]
+    crt_number = 3
+    multiples = [(4,2)]
 
     while True:
-        next_crossed = cross_list[0]
-        if next_crossed[0] > crt:
-            for n in range(crt, next_crossed[0]):
+        next_multiple = multiples[0]
+        # if the next smalles multiple is larger than the number we're currently looking at
+        # then all the numbers between the two are primes
+        # we'll report them as such and add their first multiples to the queue
+        if next_multiple[0] > crt_number:
+            for n in range(crt_number, next_multiple[0]):
                 yield n
-                heapq.heappush(cross_list, (n * n, n))
-            crt = next_crossed[0]
+                heapq.heappush(multiples, (n * n, n))
+            crt_number = next_multiple[0]
         else:
-            while next_crossed[0] == crt:
-                heapq.heappop(cross_list)
-                heapq.heappush(cross_list, (next_crossed[0] + next_crossed[1], next_crossed[1]))
-                next_crossed = cross_list[0]
-            crt += 1
+            # otherwise, the current number is not a prime, and we'll pop it
+            # from the queue, and add the next multiple to the queue
+            while next_multiple[0] == crt_number:
+                heapq.heappop(multiples)
+                heapq.heappush(multiples, (next_multiple[0] + next_multiple[1], next_multiple[1]))
+                next_multiple = multiples[0]
+            crt_number += 1
 
 if __name__ == '__main__':
     s = proper_sieve()
-    for i in range(0,100000):
+    for i in range(0,10):
         print(next(s))
